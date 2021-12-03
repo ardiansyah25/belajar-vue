ed7643f554d2f17822e0cd66698c16d47f5bb85c
Ardiansyah <aardi25@gmail.com>
2021-12-03 14:45:31 +0700

    route

--- routing/BookComponent.js
+++ routing/BookComponent.js
@@ -1,8 +1,8 @@
-export const BookComponent  = {
+export const BookComponent = {
   data() {
     return {
       books: [
-          {
+        {
           id: 99,
           title: 'C++ High Performance',
           description: 'Write code that scales across CPU registers, multi-core, and machine clusters',
@@ -10,8 +10,8 @@ export const BookComponent  = {
           publish_year: 2018,
           price: 100000,
           image: 'c++-high-performance.png'
-          },
-          {
+        },
+        {
           id: 100,
           title: 'Mastering Linux Security and Hardening',
           description: 'A comprehensive guide to mastering the art of preventing your Linux system from getting compromised',
@@ -19,8 +19,8 @@ export const BookComponent  = {
           publish_year: 2018,
           price: 125000,
           image: 'mastering-linux-security-and-hardening.png'
-          },
-          {
+        },
+        {
           id: 101,
           title: 'Mastering PostgreSQL 10',
           description: 'Master the capabilities of PostgreSQL 10 to efficiently manage and maintain your database',
@@ -28,8 +28,8 @@ export const BookComponent  = {
           publish_year: 2016,
           price: 90000,
           image: 'mastering-postgresql-10.png'
-          },
-          {
+        },
+        {
           id: 102,
           title: 'Python Programming Blueprints',
           description: 'How to build useful, real-world applications in the Python programming language',
@@ -37,20 +37,20 @@ export const BookComponent  = {
           publish_year: 2017,
           price: 75000,
           image: 'python-programming-blueprints.png'
-          },
+        },
       ],
     }
   },
-  props:['id'],
-  computed:{
-    book(){
+  props: ['id'],
+  computed: {
+    book() {
       let book = this.books.filter((book) => {
         return book.id === parseInt(this.id)
       })
-      if (book.length==0) {
+      if (book.length == 0) {
         alert("Buku tidak ditemukan")
         this.$router.push("/books")
-      }else{
+      } else {
         return book[0]
       }
     }
@@ -64,5 +64,13 @@ export const BookComponent  = {
          </li>
        </ul>
        </div>    
-  `
+  `,
+  beforeRouteLeave(to, from, next) {
+    const answer = window.confirm('Apakah and yakin akan keluar ?')
+    if (answer) {
+      next()
+    } else {
+      next(false)
+    }
+  }
 }
